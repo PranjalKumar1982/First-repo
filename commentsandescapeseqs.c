414294bd8f4ed15175696763cc4e07af9f6e8249
pritish <pritishmajumdar066@gmail.com>
2023-12-12 22:23:50 +0530

    commit1

---
+++ commentsandescapeseqs.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main() {
+
+    //this is a comment
+    
+    printf("Hello World\n");
+    return 0;
+}
---
+++ helloworld.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    printf("coding is good!!");
+    return 0;
+}
+
